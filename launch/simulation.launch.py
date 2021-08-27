64b10bc1205a66bdbb47f6d74e20fe7a7bc62afd
Pradheep-office <padhupradheep@gmail.com>
2021-08-27 14:41:27 +0200

    initia commit that loads the gazebo controllers

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -4,8 +4,9 @@
 import launch
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.event_handlers import OnProcessExit
 from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
 from launch_ros.actions import Node
 import os
@@ -70,5 +71,28 @@ def generate_launch_description():
             parameters=[{'use_sim_time': use_sim_time}],
             output='screen')
 
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
 
-    return LaunchDescription([spawn_entity, start_robot_state_publisher_cmd, teleop, gazebo, rviz])
+    load_joint_trajectory_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start', 'velocity_controller'],
+        output='screen'
+    )
+
+
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_joint_trajectory_controller],
+            )
+        ), spawn_entity, start_robot_state_publisher_cmd, teleop, gazebo, rviz])
