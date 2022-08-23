8d14d97f5a2e6a2f739c734fc231258d956c2ee0
Pradheep <padhupradheep@gmail.com>
2022-08-23 21:28:42 +0200

    update for multi_robot_simulation, now multiple robots spawn

---
+++ launch/multi_robot_simulation.launch.py
@@ -0,0 +1,58 @@
+# Neobotix GmbH
+
+import launch
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
+from launch_ros.actions import Node
+import os
+from pathlib import Path
+import xml.etree.ElementTree as ET
+import xacro
+import time
+
+MY_NO_ROBOTS = os.environ['Number_of_Robots']
+MY_NEO_ENVIRONMENT = os.environ.get('MAP_NAME', "neo_workshop")
+
+def generate_launch_description():
+    default_world_path = os.path.join(get_package_share_directory('neo_simulation2'), 'worlds', MY_NEO_ENVIRONMENT + '.world')
+
+    global MY_NO_ROBOTS 
+    if(int(MY_NO_ROBOTS) > 5):
+        print("Warn: Having more than 5 robots is not a good idea - too much overhead")
+        print("Therefore Let's spawn 5")
+        MY_NO_ROBOTS = '5'
+        time.sleep(5)
+
+    ld = LaunchDescription()
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    gazebo = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+            ),
+            launch_arguments={
+                'world': default_world_path,
+                'verbose': 'true',
+            }.items()
+        )
+
+    ld.add_action(gazebo)
+
+    for i in range(0, int(MY_NO_ROBOTS)):
+        ld.add_action(IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('neo_simulation2'), 'launch', 'simulation.launch.py')
+            ),
+            launch_arguments={
+                'world': default_world_path,
+                'verbose': 'true',
+                'use_multi_robots': 'True',
+                'x': '0', 
+                'y': str((2.0 - int(i))),
+                'namespace_robot': "robot" + str(i) 
+            }.items(),
+        ))
+
+    return ld
--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -6,8 +6,10 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration, Command, PathJoinSubstitution, FindExecutable
+from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration, Command, PathJoinSubstitution, FindExecutable, PythonExpression
 from launch_ros.actions import Node
+from launch.launch_context import LaunchContext
+from launch.conditions import IfCondition
 import os
 from pathlib import Path
 import xacro
@@ -19,43 +21,52 @@ def generate_launch_description():
     default_world_path = os.path.join(get_package_share_directory('neo_simulation2'), 'worlds', MY_NEO_ENVIRONMENT + '.world')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    robot_names = LaunchConfiguration('robot_names', default="")
+    use_multi_robots = LaunchConfiguration('use_multi_robots', default='False')
+    x = LaunchConfiguration('x', default='0')
+    y = LaunchConfiguration('y', default='0')
+    z = LaunchConfiguration('z', default='0')
+
+    robot_name = LaunchConfiguration('namespace_robot', default="")
+    context = LaunchContext()
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
         default_value='false',
         description='Use simulation (Gazebo) clock if true')
-    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
+        
     urdf = os.path.join(get_package_share_directory('neo_simulation2'), 'robots/'+MY_NEO_ROBOT+'/', MY_NEO_ROBOT+'.urdf.xacro')
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py', arguments=['-entity', robot_names, '-topic', "/robot_description",  '-robot_namespace', robot_names], output='screen')
+
+    spawn_entity = Node(
+        package = 'gazebo_ros',
+        executable = 'spawn_entity.py',
+        namespace=robot_name,
+        arguments = [
+            '-entity', robot_name,
+            '-x', x,
+            '-y', y,
+            '-z', z, 
+            '-topic', "/robot_description", 
+            '-robot_namespace', robot_name],
+        output = 'screen')
 
     start_robot_state_publisher_cmd = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
-        parameters=[{'robot_description': Command(["xacro", " ", urdf, " ", 'robot_names:=', "das/"]), 'frame_prefix':"das/"}],
+        namespace=robot_name,
+        parameters=[{'robot_description': Command([
+            "xacro", " ", urdf, " ", 'robot_name:=',
+            robot_name.perform(context)]),
+            'frame_prefix':robot_name.perform(context)}],
         arguments=[urdf])
 
-    teleop =  Node(package='teleop_twist_keyboard',executable="teleop_twist_keyboard",
-    output='screen',
-    prefix = 'xterm -e',
-    namespace=robot_names,
-    name='teleop')
-
-    relay_topic = Node(
-            package='topic_tools',
-            executable = 'relay',
-            name='relay',
-            output='screen',
-            parameters=[{'input_topic': "/das" + "/tf",'output_topic': "/tf", 'lazy': True, 'stealth ': True, 'monitor_rate': 100.0}])
-
-    relay_topics_tf_static = Node(
-            package='topic_tools',
-            executable = 'relay',
-            name='relay',
-            output='screen',
-            parameters=[{'input_topic': "/das" + "/tf_static",'output_topic': "/tf_static", 'lazy': True, 'stealth ': True, 'monitor_rate': 100.0}])
+    teleop =  Node(
+        package='teleop_twist_keyboard',executable="teleop_twist_keyboard",
+        output='screen',
+        prefix = 'xterm -e',
+        namespace=robot_name,
+        name='teleop')
 
     gazebo = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
@@ -64,7 +75,32 @@ def generate_launch_description():
             launch_arguments={
                 'world': default_world_path,
                 'verbose': 'true',
-            }.items()
+            }.items(),
+            condition=IfCondition(PythonExpression(['not ', use_multi_robots]))
         )
+
+    relay_topic = Node(
+        package='topic_tools',
+        executable = 'relay',
+        name='relay',
+        output='screen',
+        parameters=[{
+            'input_topic': robot_name.perform(context) + "/tf",
+            'output_topic': "/tf", 
+            'lazy': True, 
+            'stealth ': True, 
+            'monitor_rate': 100.0}])
+
+    relay_topics_tf_static = Node(
+        package='topic_tools',
+        executable = 'relay',
+        name='relay',
+        output='screen',
+        parameters=[{
+            'input_topic':robot_name.perform(context) + "/tf_static",
+            'output_topic': "/tf_static",
+            'lazy': True, 
+            'stealth ': True,
+            'monitor_rate': 100.0}])
 
     return LaunchDescription([start_robot_state_publisher_cmd, spawn_entity, teleop, gazebo, relay_topic, relay_topics_tf_static])
